a67f4b021d6b4231becb932e0b1c5afbc214b532
Jessica <Jessica@users-iMac.local>
2020-05-25 18:56:07 -0400

    Step 1.1.  set up web template page 4 skeleton

--- packages/web/src/App.js
+++ packages/web/src/App.js
@@ -6,6 +6,7 @@ import FooterSkeleton from './constants/Skeleton/Footer/Footer-skeleton';
 import WT1 from './constants/Skeleton/Pages/web-template-page1';
 import WT2 from './constants/Skeleton/Pages/web-template-page2';
 import WT3 from './constants/Skeleton/Pages/web-template-page3';
+import WT4 from './constants/Skeleton/Pages/web-template-page4';
 
 function App() {
 	return (
@@ -17,6 +18,7 @@ function App() {
 			<WT1 />
 			<WT2 />
 			<WT3 />
+			<WT4 />
 		</div>
 	);
 }
