6b427c5d362359088334a5212592b1afda3d3389
Jessica <Jessica@users-iMac.local>
2020-05-26 01:10:49 -0400

    Step 1.2. set up Landing Page Route

--- packages/web/src/App.js
+++ packages/web/src/App.js
@@ -1,19 +1,30 @@
-import React from 'react';
-import TemplateCatalog from './constants/Skeleton/Template-Catalog';
+import React, { useState } from 'react';
+import { Router, Route, Switch } from 'react-router-dom';
+import history from './utills/history';
 import NavBarWebSkeleton from './constants/Skeleton/NavBar/NavBar-web-skeleton.js';
 import LandingPage from './components/LandingPage/LandingPage';
 import FooterSkeleton from './constants/Skeleton/Footer/Footer-skeleton';
 
-function App() {
-	return (
-		<div className='App'>
-			{/* <TemplateCatalog /> */}
+const App = () => {
+	const [token, setToken] = useState(localStorage.getItem('token'));
 
-			<NavBarWebSkeleton />
-			<LandingPage />
-			<FooterSkeleton />
-		</div>
+	const handleLoginStateChanged = () => {
+		setToken(localStorage.getItem('token'));
+	};
+
+	return (
+		<Router history={history}>
+			<div className='App'>
+				<NavBarWebSkeleton />
+				<Route exact path='/' component={LandingPage} />
+				<FooterSkeleton />
+			</div>
+		</Router>
 	);
-}
+};
+
+// Tools for demo:
+// import TemplateCatalog from './constants/Skeleton/Template-Catalog';
+//  <TemplateCatalog />
 
 export default App;
